4b025adbeeef72f549c109111f43224de97f048d
Henrik Nero <henriknero@gmail.com>
2018-03-02 15:19:58 +0100

    New Configs

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,2 +1,4 @@
 /usr/bin/tint2 &
 feh --bg-fill /home/henrik/Pictures/Wallpapers/background.png &
+syndaemon -i 1 -k -t -d &
+synclient HorizScrollDelta=-75 VertScrollDelta=-75 HorizTwoFingerScroll=1 &
