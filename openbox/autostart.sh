48a7c7bb63e8c7941d2f7de94b53d54ba1d82345
Henrik Nero <henriknero@gmail.com>
2018-07-24 18:27:15 +0200

    Latest update

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,5 +1,6 @@
 /usr/bin/tint2 &
-feh --bg-fill /home/henrik/Pictures/Wallpapers/background.png &
-syndaemon -i 1 -k -t -d &
+feh --bg-fill /home/henrik/Pictures/Wallpapers/background.jpg &
 synclient HorizScrollDelta=-75 VertScrollDelta=-75 HorizTwoFingerScroll=1 &
-xcompmgr &
+synclient PalmDetect=1
+compton -b
+tilda -h &
