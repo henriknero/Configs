20836a305e87eb956366aba0b865fdf5795d001b
Henrik Nero <henriknero@gmail.com>
2018-09-13 17:28:23 +0200

    New stuff

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,6 +1,13 @@
 /usr/bin/tint2 &
-feh --bg-fill /home/henrik/Pictures/Wallpapers/background.jpg &
+xfdesktop &
 synclient HorizScrollDelta=-75 VertScrollDelta=-75 HorizTwoFingerScroll=1 &
 synclient PalmDetect=1
 compton -b
 tilda -h &
+syndaemon -i 1 -d -t -K &
+VGA=$(cat /sys/class/drm/card0/card0-DP-1/status)
+HMDI=$(cat /sys/class/drm/card0/card0-HDMI-A-2/status)
+if [ "$VGA"=="connected" ] && [ "$HMDI"=="connected" ]
+then
+	$HOME/.screenlayout/hemma.sh &
+fi
