1bfc181c79f0702a55be0f8b6752a67506f9a8a0
Henrik Nero2 <you@example.com>
2018-10-20 22:04:54 +0200

    Moved stuff changed copyconf

--- confs/openbox/autostart.sh
+++ confs/openbox/autostart.sh
@@ -3,7 +3,6 @@ xfdesktop &
 synclient HorizScrollDelta=-75 VertScrollDelta=-75 HorizTwoFingerScroll=1 &
 synclient PalmDetect=1
 compton -b
-tilda -h &
 syndaemon -i 1 -d -t -K &
 VGA=$(cat /sys/class/drm/card0/card0-DP-1/status)
 HMDI=$(cat /sys/class/drm/card0/card0-HDMI-A-2/status)
@@ -11,4 +10,3 @@ if [ "$VGA"=="connected" ] && [ "$HMDI"=="connected" ]
 then
 	$HOME/.screenlayout/hemma.sh &
 fi
-synergys --config $HOME/.synergy.conf &
